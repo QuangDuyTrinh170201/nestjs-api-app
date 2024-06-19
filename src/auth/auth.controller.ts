0b5a06ab907cc06bf9b3795aee8ff35e1dd2d406
QuangDuyTrinh170201 <trinhquangduy3442@gmail.com>
2024-06-20 01:59:52 +0700

    Learn NestJS v1

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDTO } from './dto';
+
+@Controller('auth')
+export class AuthController {
+  //auth service is automatically created when initializing the controller
+  constructor(private authService: AuthService) {}
+
+  @Post('register') //register a new user
+  register(@Body() body: AuthDTO) {
+    console.log(body);
+    return this.authService.register(body);
+  }
+
+  @Post('login') //login account
+  login() {
+    return this.authService.login();
+  }
+}
